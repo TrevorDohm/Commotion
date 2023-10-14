e5645127b2e1c536b05eb5e765c7284ba90c06ad
Alexander Shockley <ashockley@smu.edu>
2023-10-14 09:30:56 -0500

    Added Basic Functionality for the game

--- Commotion/GameScene.swift
+++ Commotion/GameScene.swift
@@ -21,12 +21,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         if self.motion.isDeviceMotionAvailable{
             self.motion.deviceMotionUpdateInterval = 0.1
-            self.motion.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: self.handleMotion )
+            self.motion.startDeviceMotionUpdates(to: OperationQueue.init(), withHandler: self.handleMotion )
         }
     }
     
     func handleMotion(_ motionData:CMDeviceMotion?, error:Error?){
         if let gravity = motionData?.gravity {
+//            print(gravity)
             self.physicsWorld.gravity = CGVector(dx: CGFloat(9.8*gravity.x), dy: CGFloat(9.8*gravity.y))
         }
     }
@@ -50,89 +51,96 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.startMotionUpdates()
         
         // make sides to the screen
-        self.addSidesAndTop()
-        
-        // add some stationary blocks
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.1, y: size.height * 0.25))
-        self.addStaticBlockAtPoint(CGPoint(x: size.width * 0.9, y: size.height * 0.25))
-        
-        // add a spinning block
-        self.addBlockAtPoint(CGPoint(x: size.width * 0.5, y: size.height * 0.35))
-        
-        self.addSpriteBottle()
+        self.addBounds()
+
         
         self.addScore()
         
+//        addBarAtPoint(pt: CGPoint(x: frame.midX, y: frame.minY + size.height * 0.05),ht: CGFloat(0.15),wd: CGFloat(0.025))
+        addScoringBins(numBins:5)
         self.score = 0
     }
     
+    func addScoringBins(numBins:Int = 3, ht:CGFloat = 0.15,wd:CGFloat = 0.025 ) {
+        
+        let numBars = numBins - 1
+        let binWidth = CGFloat((size.width - size.width * 0.1 ) / CGFloat(numBins))
+        
+        print("Num Bins \(numBins) | BinWidth: \(binWidth)")
+        
+        
+        for i in 1...numBars {
+            
+            let _x = binWidth * CGFloat(i) + size.width * 0.05
+            let _y = frame.minY + size.height * 0.05
+            addBarAtPoint(pt: CGPoint(x:_x , y: _y),ht: ht, wd: wd)
+            addLabelAtPt(pt: CGPoint(x:_x - (binWidth/2.0), y: _y), txt: "\(i)")
+            
+        }
+        addLabelAtPt(pt: CGPoint(x: binWidth * CGFloat(numBins) + size.width * 0.05 - binWidth/2.0, y: frame.minY + size.height * 0.05), txt: "\(numBins)")
+    }
+    
+    func addLabelAtPt(pt:CGPoint,txt:String) {
+        let binLabel = SKLabelNode(fontNamed: "Chalkduster")
+        binLabel.position = pt
+        binLabel.text = txt
+        binLabel.fontSize = 20
+        binLabel.fontColor = SKColor.blue
+        addChild(binLabel)
+    }
     // MARK: Create Sprites Functions
     func addScore(){
         
         scoreLabel.text = "Score: 0"
         scoreLabel.fontSize = 20
         scoreLabel.fontColor = SKColor.blue
-        scoreLabel.position = CGPoint(x: frame.midX, y: frame.minY)
+        scoreLabel.position = CGPoint(x: frame.midX, y: frame.maxY)
         
         addChild(scoreLabel)
     }
     
     
-    func addSpriteBottle(){
-        let spriteA = SKSpriteNode(imageNamed: "sprite") // this is literally a sprite bottle... 😎
+    func spawnBobble(){
+        let bobble = SKSpriteNode(imageNamed: "bobble") // this is literally a sprite bottle... 😎
+        let bobbleSize = 0.05
+        bobble.size = CGSize(width:size.width*bobbleSize,height:size.height * bobbleSize * 0.65) //0.65 is to keep aspect ratio correct
         
-        spriteA.size = CGSize(width:size.width*0.1,height:size.height * 0.1)
+        let randNumber = Int.random(in:30...Int(size.width))
+        print(randNumber)
+        bobble.position = CGPoint(x: CGFloat(randNumber), y: size.height * 0.75)
         
-        let randNumber = random(min: CGFloat(0.1), max: CGFloat(0.9))
-        spriteA.position = CGPoint(x: size.width * randNumber, y: size.height * 0.75)
+        bobble.physicsBody = SKPhysicsBody(rectangleOf:bobble.size)
+        bobble.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
+        bobble.physicsBody?.isDynamic = true
+        bobble.physicsBody?.contactTestBitMask = 0x00000001
+        bobble.physicsBody?.collisionBitMask = 0x00000001
+        bobble.physicsBody?.categoryBitMask = 0x00000001
         
-        spriteA.physicsBody = SKPhysicsBody(rectangleOf:spriteA.size)
-        spriteA.physicsBody?.restitution = random(min: CGFloat(1.0), max: CGFloat(1.5))
-        spriteA.physicsBody?.isDynamic = true
-        spriteA.physicsBody?.contactTestBitMask = 0x00000001
-        spriteA.physicsBody?.collisionBitMask = 0x00000001
-        spriteA.physicsBody?.categoryBitMask = 0x00000001
-        
-        self.addChild(spriteA)
+        self.addChild(bobble)
     }
     
-    func addBlockAtPoint(_ point:CGPoint){
+   
+    func addBarAtPoint(pt:CGPoint,ht:CGFloat=0.03, wd:CGFloat = 0.05){
+        let bar = SKSpriteNode()
+        bar.size = CGSize(width: size.width * wd, height: size.height * ht)
         
-        spinBlock.color = UIColor.red
-        spinBlock.size = CGSize(width:size.width*0.15,height:size.height * 0.05)
-        spinBlock.position = point
+        bar.position = CGPoint(x:pt.x,y:pt.y + (size.height * ht)/2.0)
         
-        spinBlock.physicsBody = SKPhysicsBody(rectangleOf:spinBlock.size)
-        spinBlock.physicsBody?.contactTestBitMask = 0x00000001
-        spinBlock.physicsBody?.collisionBitMask = 0x00000001
-        spinBlock.physicsBody?.categoryBitMask = 0x00000001
-        spinBlock.physicsBody?.isDynamic = true
-        spinBlock.physicsBody?.pinned = true
-        
-        self.addChild(spinBlock)
-
+        bar.color = UIColor.blue
+        bar.physicsBody = SKPhysicsBody(rectangleOf:bar.size)
+        bar.physicsBody?.isDynamic = true
+        bar.physicsBody?.pinned = true
+        bar.physicsBody?.allowsRotation = false
+        self.addChild(bar)
     }
+        
     
-    func addStaticBlockAtPoint(_ point:CGPoint){
-        let 🔲 = SKSpriteNode()
-        
-        🔲.color = UIColor.red
-        🔲.size = CGSize(width:size.width*0.1,height:size.height * 0.05)
-        🔲.position = point
-        
-        🔲.physicsBody = SKPhysicsBody(rectangleOf:🔲.size)
-        🔲.physicsBody?.isDynamic = true
-        🔲.physicsBody?.pinned = true
-        🔲.physicsBody?.allowsRotation = true
-        
-        self.addChild(🔲)
-        
-    }
     
-    func addSidesAndTop(){
+    func addBounds(){
         let left = SKSpriteNode()
         let right = SKSpriteNode()
         let top = SKSpriteNode()
+        let bot = SKSpriteNode()
         
         left.size = CGSize(width:size.width*0.1,height:size.height)
         left.position = CGPoint(x:0, y:size.height*0.5)
@@ -143,7 +151,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         top.size = CGSize(width:size.width,height:size.height*0.1)
         top.position = CGPoint(x:size.width*0.5, y:size.height)
         
-        for obj in [left,right,top]{
+        bot.size = CGSize(width: size.width, height: size.height * 0.1)
+        bot.position = CGPoint(x: size.width * 0.5, y: 0)
+        
+        for obj in [left,right,top,bot]{
             obj.color = UIColor.red
             obj.physicsBody = SKPhysicsBody(rectangleOf:obj.size)
             obj.physicsBody?.isDynamic = true
@@ -155,14 +166,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // MARK: =====Delegate Functions=====
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.addSpriteBottle()
+        self.spawnBobble()
     }
     
-    func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node == spinBlock || contact.bodyB.node == spinBlock {
-            self.score += 1
-        }
-    }
+    
     
     // MARK: Utility Functions (thanks ray wenderlich!)
     func random() -> CGFloat {
