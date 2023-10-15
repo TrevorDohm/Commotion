afafecbb5d7b2e388993cdb8dcd277ab5958f7a6
Trevor Dohm <trevordohm@gmail.com>
2023-10-14 21:00:13 -0700

    Finished With Comments! Will - UIView Handling

--- Commotion/ViewController.swift
+++ Commotion/ViewController.swift
@@ -5,165 +5,189 @@
 //  Created by Eric Larson on 9/6/16.
 //  Copyright © 2016 Eric Larson. All rights reserved.
 //
+//  Lab Three: Core Motion, SpriteKit (CMSK)
+//  Trevor Dohm, Alex Shockley, Will Landin, Ray Irani
+//
 
+// Import Statements
 import UIKit
 import CoreMotion
 
+// Main View Controller (Front Page)
 class ViewController: UIViewController {
     
     // MARK: Class Variables
     
+    // Note: "Let" Used For Constants, "Var" For Variables
+    
+    // Obtain Core Motion Objects
     let activityManager = CMMotionActivityManager()
     let pedometer = CMPedometer()
     let motion = CMMotionManager()
-    let calendar = Calendar.current
     
-    var stepsToday: Float = 0.0 {
+    // Convenience Variable For Readability
+    // Handles Persistence Across Restarts
+    let defaults = UserDefaults.standard
+    
+    // Initialize Timer Here - Makes Deallocation Easier
+    var stepUpdateTimer: Timer?
+    
+    // Note: "WillSet" Used When Method Does Not Need The Variable,
+    // "DidSet" For When Method References Variable In It's Assignment
+    
+    // Initialize Steps Today
+    var stepsToday: Int = 0 {
         willSet(newStepsToday) {
+            
+            // Whenever Steps Today Changes, Update View
             DispatchQueue.main.async {
-                self.stepsLabel.text = "Steps Today: \(newStepsToday)"
-                self.checkGoalStatus()
+                self.stepsTodayLabel.text = "Steps Today: \(newStepsToday)"
+                self.updateGoalStatus()
             }
         }
     }
     
-    var stepsYesterday: Float = 0.0 {
+    // Initialize Steps Yesterday
+    var stepsYesterday: Int = 0 {
         willSet(newStepsYesterday) {
+            
+            // Whenever Steps Yesterday Changes, Update View
+            // Note: This Also Changes When The Day Changes (Midnight)
             DispatchQueue.main.async {
                 self.stepsYesterdayLabel.text = "Steps Yesterday: \(Int(newStepsYesterday))"
             }
         }
     }
     
-    var dailyGoal: Float = 10000.0 {
+    // Initialize Daily Goal (10000 First Time Opening App)
+    // CDC Recommends Most Adults To Get 10000 Steps! Are You
+    // Getting 10000 Steps Eric? Because I'm Surely Not :3
+    var dailyGoal: Int = 10000 {
         didSet {
-            if dailyGoal != 0 {
-                UserDefaults.standard.set(dailyGoal, forKey: "DailyGoal")
+            
+            // Maintain Daily Goal
+            if self.dailyGoal != 0 {
+                defaults.set(self.dailyGoal, forKey: "DailyGoal")
             }
+            
+            // Update View (Like Above)
             DispatchQueue.main.async{
-                self.checkGoalStatus()
+                self.updateGoalStatus()
             }
         }
     }
     
     // MARK: UI Elements
     
-    @IBOutlet weak var stepsSlider: UISlider!
-    @IBOutlet weak var stepsLabel: UILabel!
-    @IBOutlet weak var isWalking: UILabel!
+    @IBOutlet weak var goalSlider: UISlider!
+    @IBOutlet weak var stepsTodayLabel: UILabel!
+    @IBOutlet weak var activityLabel: UILabel!
     @IBOutlet weak var stepsYesterdayLabel: UILabel!
     @IBOutlet weak var stepsGoalLabel: UILabel!
     @IBOutlet weak var playGameButton: UIButton!
-    @IBAction func sliderValueChanged(_ sender: UISlider) {
-        self.dailyGoal = sender.value
-    }
     @IBOutlet weak var viewInterface: ViewInterface!
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        self.dailyGoal = Int(sender.value)
+    }
     
     // MARK: View Lifecycles
     
+    // Run When View Loads (With Super Method)
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let savedGoal = UserDefaults.standard.value(forKey: "DailyGoal") as? Float {
+        // Pull Daily Goal From Previous Run
+        if let savedGoal = defaults.value(forKey: "DailyGoal") as? Int {
             dailyGoal = savedGoal
-        } else {
-            dailyGoal = 10000.0
         }
         
-        stepsSlider.value = dailyGoal
+        // Update Goal Slider With Daily Goal
+        goalSlider.value = Float(dailyGoal)
         
-        self.stepsToday = 0.0
+        // Start Activity Monitoring (Whatcha Doin?)
         self.startActivityMonitoring()
-        self.startPedometerMonitoring()
-        self.fetchDateSteps()
+        
+        // Fetch Pedometer Information Frequently
+        stepUpdateTimer = Timer.scheduledTimer(timeInterval: 0.05, target: self,
+            selector: #selector(self.fetchSteps),
+            userInfo: nil, repeats: true)
     }
     
-    // MARK: Raw Motion Functions
-    
-    func startMotionUpdates(){
-        if self.motion.isDeviceMotionAvailable {
-            self.motion.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: self.handleMotion)
-        }
-    }
-    
-    func handleMotion(_ motionData: CMDeviceMotion?, error: Error?) {
-        if let gravity = motionData?.gravity {
-            let rotation = atan2(gravity.x, gravity.y) - Double.pi
-            DispatchQueue.main.async {
-                self.isWalking.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
-            }
-        }
+    // Handle Timer Deallocation
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stepUpdateTimer?.invalidate()
+        stepUpdateTimer = nil
     }
     
     // MARK: Activity Methods
     
-    func startActivityMonitoring(){
+    // Check Activity Availability, Update From Operation Queue (Not Main Queue)
+    func startActivityMonitoring() {
         if CMMotionActivityManager.isActivityAvailable() {
             self.activityManager.startActivityUpdates(to: OperationQueue.main, withHandler: self.handleActivity)
         }
     }
     
+    // Obtain Activity Type From Core Motion, Display With Relevant Emoji
     func handleActivity(_ activity: CMMotionActivity?) {
         if let unwrappedActivity = activity {
-            let activityStatus = activityDescription(activity: unwrappedActivity)
             DispatchQueue.main.async {
-                self.isWalking.text = activityStatus
+                self.activityLabel.text = "You're " + {
+                    if unwrappedActivity.walking { return "Walking! 🚶‍♂️" }
+                    if unwrappedActivity.stationary { return "Not Moving! 🛑" }
+                    if unwrappedActivity.running { return "Running! 🏃‍♂️" }
+                    if unwrappedActivity.cycling { return "Cycling! 🚴‍♂️" }
+                    if unwrappedActivity.automotive { return "Driving! 🚗" }
+                    return "Doing Something...? 🤔"
+                }()
             }
         }
     }
-    
-    func activityDescription(activity: CMMotionActivity) -> String {
-        if activity.walking { return "Walking" }
-        if activity.stationary { return "Still" }
-        if activity.running { return "Running" }
-        if activity.cycling { return "Cycling" }
-        if activity.automotive { return "Driving" }
-        return "Unknown"
-    }
-    
-    // MARK: Pedometer Methods
-    
-    func startPedometerMonitoring() {
-        if CMPedometer.isStepCountingAvailable() {
-            pedometer.startUpdates(from: Date(), withHandler: handlePedometer)
-        }
-    }
-    
-    func handlePedometer(_ pedData: CMPedometerData?, error: Error?) {
-        if let steps = pedData?.numberOfSteps {
-            self.stepsToday = steps.floatValue
-        }
-    }
 
-    func fetchDateSteps() {
+    // MARK: Update Methods
+
+    // Main Workhorse - Fetch Date Information, Query Pedometer With Said Information
+    @objc func fetchSteps() {
         
-        // Get Start Of Today (12:00 AM - Midnight)
+        // Obtain Current Calendar (Today, Yesterday, Etc.)
+        let calendar = Calendar.current
+        
+        // Get Start Of Today (12:00 AM = Midnight)
         let startOfToday = calendar.startOfDay(for: Date())
         
         // Get Start Of Yesterday (Above, Yesterday)
         guard let startOfYesterday = calendar.date(byAdding: .day, value: -1, to: startOfToday) else { return }
         
         // Get End Of Yesterday (One Second Before Midnight Today)
+        // Well, If We Are Being Exact This Is Correct... Just Saying...
         guard let endOfYesterday = calendar.date(byAdding: .second, value: -1, to: startOfToday) else { return }
         
         // Fetch Steps For Yesterday
         pedometer.queryPedometerData(from: startOfYesterday, to: endOfYesterday) { (data, error) in
             if let steps = data?.numberOfSteps {
-                self.stepsYesterday = steps.floatValue
+                self.stepsYesterday = steps.intValue
             }
         }
         
         // Fetch Steps For Today
         pedometer.queryPedometerData(from: startOfToday, to: Date()) { (data, error) in
             if let steps = data?.numberOfSteps {
-                self.stepsToday = steps.floatValue
+                self.stepsToday = steps.intValue
             }
         }
+        
+        // UPDATE VIEW HERE! (MAKE SURE TO CALL MAIN QUEUE)
+//        DispatchQueue.main.async{
+//
+//        }
+        
     }
     
-    func checkGoalStatus() {
+    // Update Goal Status - Need To Call From Main Queue
+    func updateGoalStatus() {
         let remainingSteps = max(0, Int(self.dailyGoal) - Int(self.stepsToday))
-        self.stepsGoalLabel.text = "Steps to Goal: \(remainingSteps)"
+        self.stepsGoalLabel.text = "Steps Remaining -> Goal: \(remainingSteps)"
         playGameButton.isHidden = remainingSteps > 0
     }
 }
